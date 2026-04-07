050137fe2b277f2808c41d2c5b66128c46c6b988
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-04-07 09:27:42 +0530

    malloc function

---
+++ LINKED LIST/mallocFucntion.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int *ptr;
+
+    ptr = (int *)malloc(5 * sizeof(int));
+
+    if(ptr == NULL){
+        printf("Memory Allocation failed\n");
+        return 1;
+    }
+
+    for(int i = 0; i < 5; i++){
+        ptr[i] = i * 10;
+    }
+
+    printf("Allocated memory(malloc):");
+    for(int i = 0; i < 5 ; i++){
+        printf("%d ",ptr[i]);
+    }
+    printf("\n");
+}
