6e14dfa6ef5f5e574f6fe48ab54bd216ae15eae2
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-04-07 08:07:37 +0530

    Link Last node into head node (Circular Linked List)

---
+++ LINKED LIST/CircularLinkedeList.c
@@ -0,0 +1,31 @@
+// Circular linked list vo linked list hoti hai jiska last node NULL ki jagah First node ko point krta hai
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node * next;
+};
+
+int main(){
+
+    struct node * head;
+    struct  node * second;
+    struct node * third;
+
+    head = (struct node* )malloc(sizeof(struct node));
+    second = (struct node* )malloc(sizeof(struct node));
+    third = (struct node* )malloc(sizeof(struct node));
+
+    head->data = 12;
+    head->next = second;
+
+    second->data = 13;
+    second->next = third;
+
+    third->data = 14;
+    third->next = head;
+
+
+}
