e9061207b013d605cfd542864bf9959b339e15ac
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-04-09 08:06:27 +0530

    Insertion at end successfull

--- ARRAY/insertionArray.c
+++ ARRAY/insertionArray.c
@@ -1,36 +1,30 @@
 #include<stdio.h>
 
-void PrintElemnt(int arr[],int n){
+int PrintElement(int arr[],int n){
     for(int i = 0; i < n; i++){
         printf("%d ",arr[i]);
     }
     printf("\n");
 }
 
-int InsertioAtBegin(int new_element,int n,int arr[]){
-    for(int i = n; i > 0;i--){
-        arr[i] = arr[i - 1];
-         }
-         arr[0] = new_element;
-         n++;
+int InsertionAtEnd(int arr[],int n,int new_element){
 
-         return n+1;
+    arr[n] = new_element;
+    n++;
+    return n;
 }
 int main(){
 
     int arr[10] = {12,34,56,78,90};
     int n = 5;
 
-    printf("Original Array are : ");
-    PrintElemnt(arr,n);
+    printf("Original element are : ");
+    PrintElement(arr,n);
     int new_element;
-
-    printf("Enter new element Insert at begin : ");
+    printf("Enter new element : ");
     scanf("%d",&new_element);
+    printf("After insertion at end : ");
+    n = InsertionAtEnd(arr,n,new_element);
+    PrintElement(arr,n);
 
-    n = InsertioAtBegin(new_element,n,arr);
-    printf("After Insertion in array : ");
-    PrintElemnt(arr,n);
-
-  
 }
