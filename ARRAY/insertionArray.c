8436e7b20dab52da413bf75adae706860977e5fc
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-04-09 08:28:59 +0530

    Insertion at specific position done

--- ARRAY/insertionArray.c
+++ ARRAY/insertionArray.c
@@ -1,30 +1,47 @@
+// insertoin at specific position
+
 #include<stdio.h>
 
-int PrintElement(int arr[],int n){
+int printelement(int arr[],int n){
+     printf("Original array are : " );
     for(int i = 0; i < n; i++){
         printf("%d ",arr[i]);
     }
     printf("\n");
 }
 
-int InsertionAtEnd(int arr[],int n,int new_element){
+int InsertionPosition(int arr[],int n,int new_element,int position){
 
-    arr[n] = new_element;
+    if(position < 0 || position > n){
+        printf("Invalid position\n");
+    }
+
+    for(int i = n; i > position; i--){
+        arr[i] = arr[i - 1];
+    }
+    arr[position] = new_element;
     n++;
+
     return n;
 }
 int main(){
 
-    int arr[10] = {12,34,56,78,90};
+    int arr[10] = {34,54,32,56,78};
+
     int n = 5;
 
-    printf("Original element are : ");
-    PrintElement(arr,n);
+   printelement(arr,n);
+
     int new_element;
     printf("Enter new element : ");
     scanf("%d",&new_element);
-    printf("After insertion at end : ");
-    n = InsertionAtEnd(arr,n,new_element);
-    PrintElement(arr,n);
 
+    int position;
+    printf("Enter position : ");
+    scanf("%d",&position);
+
+    printf("After Insertion : ");
+     n = InsertionPosition(arr,n,new_element,position);
+    printelement(arr,n);
+    
 }
