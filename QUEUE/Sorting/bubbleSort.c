796ff41b219a12e9cfc8baf3c79f4f741ceb5fa7
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-02 09:39:26 +0530

    Create function for bubble sorting

--- QUEUE/Sorting/bubbleSort.c
+++ QUEUE/Sorting/bubbleSort.c
@@ -1,8 +1,29 @@
 #include<stdio.h>
+
+int OriginalArray(int arr[],int n){
+    for(int i = 0; i < n; i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+int BubbleSort(int arr[],int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 int main(){
 
     int arr[] = {34,12,45,23,67,54,32};
     int n = sizeof(arr) / sizeof(arr[0]);
     printf("Original array : ");
-    
+    OriginalArray(arr,n);
+    printf("After sorting array : ");
+    BubbleSort(arr,n);
 }
