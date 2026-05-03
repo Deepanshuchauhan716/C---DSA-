c62370ad3ab95b2a0d061baf1672b6ee48f6c8b5
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-03 13:12:18 +0530

    Print all element function

--- Sorting/selectionSort.c
+++ Sorting/selectionSort.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 
-int OriginalArray(){
-    
+int OriginalArray(int arr[],int n){
+    for(int i = 0; i < n; i++){
+        printf("%d ",arr[i]);
+    }
 }
 int main(){
 
     int arr[] = {12,1,34,32,45,67,54,33,56,90,65};
     int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Original array");
+    printf("Original array : ");
+    OriginalArray(arr,n);
 }
