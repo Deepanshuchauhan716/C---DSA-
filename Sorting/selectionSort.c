24b28b66ed58e0e5aeb48d2192556e31bd68c996
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-03 13:10:16 +0530

    SelectionSort

---
+++ Sorting/selectionSort.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int OriginalArray(){
+    
+}
+int main(){
+
+    int arr[] = {12,1,34,32,45,67,54,33,56,90,65};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array");
+}
