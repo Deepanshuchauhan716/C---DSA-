d4f464647e2bd5ac1e4c19ebb27ddbc751f7c0ed
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-08 19:11:08 +0530

    Insert at begin

--- ARRAY/insertionn.c
+++ ARRAY/insertionn.c
@@ -1,45 +1,40 @@
+// Insetion at begining
+
 #include<stdio.h>
 
-void OriginalArray(int arr[], int n){
+void OriginalArray(int arr[],int n){
     for(int i = 0; i < n; i++){
         printf("%d ",arr[i]);
     }
     printf("\n");
 }
 
-int insertion(int arr[],int n,int position, int value){
+int InsertAtBegin(int arr[],int n,int value){
 
-    if(position > n || position < 0){
-        printf("Invalid position\n");
+    for(int i = n; i > 0; i--){
+        arr[i] = arr[i - 1];
     }
 
-    for(int i = n ;i  > position ;i--){
-        arr[i + 1] = arr[i];
-    }
-
-    arr[position] = value;
+    arr[0] = value;
     n++;
+
     return n;
 }
 
 int main(){
 
-    int arr[10] = {12,34,56,78,90};
+    int arr[] = {12,34,56,78,90};
     int n = 5;
 
-    printf("Original Array : ");
+    printf("Original array : ");
     OriginalArray(arr,n);
 
-    int position, value;
+    int value;
+
     printf("Enter value : ");
     scanf("%d",&value);
 
-    printf("Enter position : ");
-    scanf("%d",&position);
-    
-    n = insertion(arr,n,position,value);
-    printf("After Insertion : ");
-
-    
+    n = InsertAtBegin(arr,n,value);
+    printf("After insertion : ");
     OriginalArray(arr,n);
 }
