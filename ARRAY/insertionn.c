bc71e50f3e3595aa968b356e709901e6764f21fd
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-08 18:40:57 +0530

    Code complete successfully

--- ARRAY/insertionn.c
+++ ARRAY/insertionn.c
@@ -7,11 +7,39 @@ void OriginalArray(int arr[], int n){
     printf("\n");
 }
 
+int insertion(int arr[],int n,int position, int value){
+
+    if(position > n || position < 0){
+        printf("Invalid position\n");
+    }
+
+    for(int i = n ;i  > position ;i--){
+        arr[i + 1] = arr[i];
+    }
+
+    arr[position] = value;
+    n++;
+    return n;
+}
+
 int main(){
 
-    int arr[] = {12,34,56,78,90};
+    int arr[10] = {12,34,56,78,90};
     int n = 5;
 
     printf("Original Array : ");
+    OriginalArray(arr,n);
+
+    int position, value;
+    printf("Enter value : ");
+    scanf("%d",&value);
+
+    printf("Enter position : ");
+    scanf("%d",&position);
+    
+    n = insertion(arr,n,position,value);
+    printf("After Insertion : ");
+
+    
     OriginalArray(arr,n);
 }
