1bb143b6c49aa62d6348f5b587a40b14f04246a6
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-11 13:00:35 +0530

    Create function : Print all array element

--- Sorting/bubbleSort.c
+++ Sorting/bubbleSort.c
@@ -1,4 +1,15 @@
 #include<stdio.h>
+
+void OriginalArray(int arr[],int n){
+    for(int i = 0; i < n; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
 int main(){
-    
+
+    int arr[] = {42, 7, 19, 88, 3, 56, 21, 90, 14, 67, 1, 35, 72, 9, 50, 28, 61, 11, 84, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original Array : ");
+    OriginalArray(arr,n);
 }
