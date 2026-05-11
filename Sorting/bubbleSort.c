e4e5dc30bd281a20b27b199788befa57a45bdca2
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-11 12:56:40 +0530

    Bubble sort revision

--- Sorting/bubbleSort.c
+++ Sorting/bubbleSort.c
@@ -1,31 +1,4 @@
 #include<stdio.h>
-
-int OriginalArray(int arr[],int n){
-    for(int i = 0; i < n; i++){
-        printf("%d ",arr[i]);
-    }
-    printf("\n");
-}
-
-int BubbleSort(int arr[],int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n - i - 1; j++){
-            if(arr[j] > arr[j + 1]){
-                int temr = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temr;
-            }
-        }
-    }
-}
-
 int main(){
-
-    int arr[] = {34,12,45,23,67,54,32};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Original array : ");
-    OriginalArray(arr,n);
-    printf("After sorting array : ");
-    BubbleSort(arr,n);
-    OriginalArray(arr,n);
+    
 }
