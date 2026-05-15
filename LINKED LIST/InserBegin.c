ae465c4f036c63fada8edb9ea672b4ddfa103242
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-15 16:59:11 +0530

    Code complete successfully

--- LINKED LIST/InserBegin.c
+++ LINKED LIST/InserBegin.c
@@ -6,6 +6,23 @@ struct node {
     struct node * next;
 };
 
+void display(struct node * head){
+    while(head != NULL){
+        printf("%d ",head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+struct node *  InsertBeginLL(struct node * head, int value){
+    struct node * Newnode = (struct node *)malloc(sizeof(struct node));
+    Newnode->data = value;
+    Newnode->next = head;
+
+    head = Newnode;
+
+    return head;
+}
 int main(){
 
     int value;
@@ -21,5 +38,16 @@ int main(){
 
     third->data = 56;
     third->next = NULL;
+
+    printf("Before insertion : ");
+    display(head);
+
+    printf("Enter value : ");
+    scanf("%d",&value);
+
+    printf("After insertion : ");
+    head = InsertBeginLL(head,value);
+    display(head);
+
 
 }
