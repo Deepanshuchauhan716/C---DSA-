bc0f41d63b4dbd099e1a9200de814581840f6df7
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-15 16:34:21 +0530

    CODE COMPLETE SUCCESSFULLY

--- Stack/LinkedListStack.c
+++ Stack/LinkedListStack.c
@@ -1,15 +1,35 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 struct node{
     int data;
     struct node * next;
 };
 
-void InsertAtSpecific(){
-    
+void display(struct node * head){
+    while(head != NULL){
+        printf("%d ",head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void InsertAtSpecific(struct node * head,int value,int index){
+
+    struct node * NewNode = (struct node*)malloc(sizeof(struct node));
+    struct node * temp = head;
+
+    NewNode->data = value;
+
+    for(int i = 1; i < index - 1; i++){
+        temp = temp->next;
+    }
+    NewNode->next = temp->next;
+    temp->next = NewNode;
 }
 
 int main(){
+
+    int value,index;
     struct node * head = (struct node *)malloc(sizeof(struct node));
     struct node * second = (struct node *)malloc(sizeof(struct node));
     struct node * third = (struct node *)malloc(sizeof(struct node));
@@ -23,6 +43,16 @@ int main(){
     third->data = 56;
     third->next = NULL;
 
+    printf("Before insertion : ");
+    display(head);
 
+     printf("Enter value : ");
+     scanf("%d",&value);
+
+     printf("Enter index : ");
+     scanf("%d",&index);
+    printf("After insertion : ");
+    InsertAtSpecific(head,value,index);
+    display(head);
 
 }
