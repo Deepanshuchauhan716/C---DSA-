e7979ac6094bc3f0a148d7820d55b7b8c1199938
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-15 16:11:28 +0530

    INSERT IN LL : SPECIFIC POS

---
+++ Stack/LinkedListStack.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+struct node{
+    int data;
+    struct node * next;
+};
+
+void InsertAtSpecific(){
+    
+}
+
+int main(){
+    struct node * head = (struct node *)malloc(sizeof(struct node));
+    struct node * second = (struct node *)malloc(sizeof(struct node));
+    struct node * third = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 12;
+    head->next = second;
+
+    second->data = 34;
+    second->next = third;
+
+    third->data = 56;
+    third->next = NULL;
+
+
+
+}
