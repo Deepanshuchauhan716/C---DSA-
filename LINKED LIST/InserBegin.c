d3c88af4b7d6b4e7e4b65fa29b5b2830f12970a6
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-15 16:44:09 +0530

    INSERT AT BEGIN : ll

---
+++ LINKED LIST/InserBegin.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node * next;
+};
+
+int main(){
+
+    int value;
+    struct node * head = (struct node *)malloc(sizeof(struct node));
+    struct node * second = (struct node *)malloc(sizeof(struct node));
+    struct node * third = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 12;
+    head->next = second;
+
+    second->data = 34;
+    second->next = third;
+
+    third->data = 56;
+    third->next = NULL;
+
+}
