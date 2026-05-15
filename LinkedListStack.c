8376483a0e9478f1ae2ea645f22dc0600a0633fb
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-15 15:44:37 +0530

    Create Function : PUSH & POP

--- LinkedListStack.c
+++ LinkedListStack.c
@@ -5,6 +5,36 @@ struct node{
     struct node * next;
 };
 
+struct node * top = NULL;
+void push(int value){
+    struct node * NewNode = (struct node *)malloc(sizeof(struct node));
+    if(!NewNode){
+        printf("Heap overflow !\n");
+    }
+    NewNode->data = value;
+    NewNode->next = top;
+    top = NewNode;
+    printf("%d is push into stack",value);
+}
+void pop(){
+    if(top == NULL){
+        printf("Stack is empty\n");
+    }else{
+        struct node * temp = top;
+        printf("%d element is popped\n",temp->data);
+        top = top->next;
+        free(temp);
+    }
+}
+void peek(){
+
+}
+void display(){
+
+}
+
 int main(){
+
+    int choice,value;
 
 }
