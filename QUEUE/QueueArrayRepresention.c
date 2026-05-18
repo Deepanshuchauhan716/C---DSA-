d1f6608cf2d2571838953f101507abb6b6d64694
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-05-18 18:15:45 +0530

    REPRESENTATION : queue using array

--- QUEUE/QueueArrayRepresention.c
+++ QUEUE/QueueArrayRepresention.c
@@ -1,44 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 #define SIZE 5
-int queue[SIZE];
 int front = -1,tail = -1;
 
-int IsEmpty(){
-    return(front == -1);
+int Isempty(){
+    return (front == -1); 
 }
 
 int IsFull(){
     return(tail == SIZE - 1);
 }
 
-void Push(int value){
-    if(IsFull()){
-        printf("Queue is Full !\n");
-        return;
-    }
-    if(IsEmpty()){
-        front = 0;
-        queue[++tail] = value;
-        printf("inserted %d\n",value);
-    }
+int push(){
+    
 }
+int main(){
 
-void pop(){
-    if(IsEmpty()){
-        printf("Queue is empty !\n");
-        return;
-    }
-    printf("Deleted element %d :",queue[front]);
-    if(front == tail){
-        front = tail = - 1;
-    }else{
-        front++;
-    }
-}
-
-void display(){
-    if(IsEmpty()){
-        printf("Empty\n");
-        return;
-    }
 }
